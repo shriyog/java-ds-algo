1afad7f39e73120ae219774d13f95020f2e1860c
shriyog <shriyog1995@gmail.com>
2019-01-09 14:14:08 +0530

    Add unit tests for data structure - Binary tree

---
+++ src/test/java/com/github/shriyog/datastructures/tree/TreeTest.java
@@ -0,0 +1,58 @@
+package com.github.shriyog.datastructures.tree;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Shriyog Ingale 09-Jan-2019
+ */
+public class TreeTest {
+
+	private BinaryTree tree = new BinaryTree();
+
+	@Before
+	public void setup() {
+		// tree = new BinaryTree();
+		// ______1
+		// ___ 2 __3
+		// __ 4_5
+		// tree.add(1);
+		// tree.add(2);
+		// tree.add(3);
+		// tree.add(4);
+		tree.root = new TreeNode(1);
+		tree.root.setLeft(new TreeNode(2));
+		tree.root.setRight(new TreeNode(3));
+		tree.root.getLeft().setLeft(new TreeNode(4));
+		tree.root.getLeft().setRight(new TreeNode(5));
+	}
+
+	@Test
+	public void testPreOrderTraversal() {
+		Integer[] expected = { 1, 2, 4, 5, 3 };
+		System.out.println("PreOrder: " + tree.preOrder());
+		Assert.assertArrayEquals(expected, tree.preOrder().toArray());
+	}
+
+	@Test
+	public void testInOrderTraversal() {
+		Integer[] expected = { 4, 2, 5, 1, 3 };
+		System.out.println("InOrder: " + tree.inOrder());
+		Assert.assertArrayEquals(expected, tree.inOrder().toArray());
+	}
+
+	@Test
+	public void testPostOrderTraversal() {
+		Integer[] expected = { 4, 5, 2, 3, 1 };
+		System.out.println("PostOrder: " + tree.postOrder());
+		Assert.assertArrayEquals(expected, tree.postOrder().toArray());
+	}
+
+	@Test
+	public void testBreadthFirstTraversal() {
+		Integer[] expected = { 1, 2, 3, 4, 5 };
+		System.out.println("LevelOrder: " + tree.breadthFirst());
+		Assert.assertArrayEquals(expected, tree.breadthFirst().toArray());
+	}
+}
